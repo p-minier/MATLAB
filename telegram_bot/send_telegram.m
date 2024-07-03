5865b98dd9529dadaf4537697378eab4eba6872a
p-minier <pierreminier64@gmail.com>
2024-07-03 15:29:34 +0200

    Message automatique

--- telegram_bot/send_telegram.m
+++ telegram_bot/send_telegram.m
@@ -1,6 +1,10 @@
 function send_telegram(Message)
     load secret.mat
     MyBot=telegram_bot(bot_ID);
+    if nargin == 0
+        Stack = dbstack();
+        Message = "La simulation <" + Stack(2).name + "> est terminée.";
+    end
     warning ("off", "all");
     MyBot.sendMessage(ChatID, Message);
     warning ("on", "all");
