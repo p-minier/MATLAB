acea3be4a72363a74c4ae6678674d309c6aa44bf
Pierre Minier <pierreminier64@gmail.com>
2026-01-26 10:25:39 +0100

    GetColor

---
+++ figure/GetColor.m
@@ -0,0 +1,15 @@
+function color = GetColor(k)
+    % Palette de couleur
+    colors = [
+              "#0072B2";  % bleu brillant (1)
+              "#E69F00";  % orange vif (2)
+              "#F0E442";  % jaune beurre (3)
+              "#000000";  % noir (4)
+              "#009E73";  % vert forêt (5)
+              "#56B4E9";  % bleu ciel (6)
+              "#D55E00";  % orange brûlé (7)
+              "#CC79A7"   % rose (8)
+              ];
+    color = colors(k);
+end
+
